353aa67161ed478c1bedf185538d5c319ee7244e
JesusCh <jesus.chavez.caro@uabc.edu.mx>
2019-05-10 19:34:13 -0700

    Integracion de POSTGRES, a partir de aqui se utiliza un nuevo gestor de dts

--- src/kywork/settings.py
+++ src/kywork/settings.py
@@ -94,11 +94,14 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kywork',
+        'USER': 'postgres',
+        'PASSWORD': 'kywork',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
